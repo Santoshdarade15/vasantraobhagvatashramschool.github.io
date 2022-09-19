d2c6501a9271e9926f946a584ae2de97408b2324
Santosh Darade <113831335+VBASchool@users.noreply.github.com>
2022-09-19 20:45:29 +0530

    Changes added
    Photos added

---
+++ js/school.js
@@ -0,0 +1,12 @@
+
+console.log('ddddddsds');
+function sendQuery(){
+    console.log('ddddd'); 
+    let gname = $('#gname').val();  
+    let sname = $('#sname').val();  
+    let gmail = $('#gmail').val();  
+    let message = $('#message').val();  
+    let sclass = $('#sclass').val();  
+    console.log('ddddd', gname); 
+
+}
